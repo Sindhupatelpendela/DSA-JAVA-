bbb15a188a102d67974a50a6c0d07eea51507bae
PENDELA SINDHU <sindhupatel0002@gmail.com>
2026-06-15 12:18:28 +0000

    Day 1 - basics of java Sum of digits using If the input is: int num = 1234; ➡️ Use the modulus (%) and division (/) approach because the digits are not stored separately.

--- ARRAYS/LargestElement.java
+++ ARRAYS/LargestElement.java
@@ -1,13 +1,14 @@
+// Problem: Largest Element in Array
+// Topic: Arrays
+// Difficulty: Easy
+
 public class LargestElement {
-
     public static void main(String[] args) {
-
         int[] nums = {3, 3, 6, 1};
 
         int max = nums[0];
 
         for(int i = 1; i < nums.length; i++) {
-
             if(nums[i] > max) {
                 max = nums[i];
             }
---
+++ AllBasics/SumOfDigits.java
@@ -0,0 +1,16 @@
+package AllBasics;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 1234;
+        int sum = 0;
+
+        while(num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println(sum);
+    }
+    
+}
